a90bfec3995fe272f8199469e7dcb73ec12a042b
Name???? <emanuelsul53@gmail.com>
2024-05-26 00:02:05 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const yesBtn = document.querySelector('#yesBtn');
-
-yesBtn.addEventListener('click',function () {
-    alert('Pinche Furro 0.o')
-});
-
 const noBtn = document.querySelector('#noBtn');
 
-noBtn.addEventListener('mouseover',function () {
+noBtn.addEventListener('click',function () {
+    alert('Pinche Posser :v')
+});
+
+const siBtn = document.querySelector('#siBtn');
+
+siBtn.addEventListener('mouseover',function () {
     const randomX = parseInt(Math.random()*100);
     const randomY = parseInt(Math.random()*100);
-    noBtn.style.setProperty('top',randomY+'%');
-    noBtn.style.setProperty('lwft',randomX+'%');
-    noBtn.style.setProperty('transform'`traslate(-${randomX}%-${randomY}%)`);
+    siBtn.style.setProperty('top',randomY+'%');
+    siBtn.style.setProperty('left',randomX+'%');
+    siBtn.style.setProperty('transform'`traslate(-${randomX}%-${randomY}%)`);
      
 })
